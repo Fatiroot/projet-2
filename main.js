04579b8fc047e9ddd84a4160b7b569275569366d
Rassili <adnan.rassili47@gmail.com>
2023-10-22 23:15:18 +0100

    navbar commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function navslider() {
+   const menu =document.querySelector('.menu');
+    const nav = document.querySelector('.burger');
+    
+menu.addEventListener('click' , function openbar(){ nav.classList.toggle('nav-active');}
+);
+    
+}
+navslider();
